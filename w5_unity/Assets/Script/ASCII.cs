a11504aaee135b7d77b4c3e1437c679784d8d88a
LAPTOP-2HQ0J53J\qiuyu <guoqiuyu23@gmail.com>
2021-03-14 19:18:30 -0400

    no message

---
+++ w5_unity/Assets/Script/ASCII.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ASCII : MonoBehaviour
+{
+    public GameObject personEat;
+    public GameObject Wall;
+    public float xOffset;
+    public float yOffset;
+
+    public string file_name;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadMap();
+    }
+
+    void LoadMap()
+    {
+        string current_file_path = Application.dataPath + "/Maps/" + file_name;
+        // string fileText = File.ReadAllText(current_file_path);
+        string[] fileLines = File.ReadAllLines(current_file_path);
+        for(int y=0; y<fileLines.Length;y++)
+        {
+            string fileText = fileLines[y]; 
+            char[] characters = fileText.ToCharArray();
+            for (int x = 0; x < characters.Length; x++)
+            {
+                char c = characters[x];
+                if (c == 'w')
+                {
+                    GameObject newWall;
+                    newWall = Instantiate(Wall);
+                    newWall.transform.position = new Vector2(xOffset + x, yOffset-y);
+                }
+            }
+        }
+    } 
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ w5_unity/Assets/Script/Eat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    private float personWidth ;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        personWidth = (float) 0.5;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector2(personWidth, (float)0.7);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(other.gameObject);
+        personWidth += (float)0.1;
+        Debug.Log("eat");
+    }
+
+ 
+}
